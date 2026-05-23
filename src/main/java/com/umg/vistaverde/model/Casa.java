2c153b38503928cb2b29173ff3ef27ff4566e7df
andresmoln <andresmoln02@gmail.com>
2026-05-23 07:12:37 -0600

    Agregar clase Casa al modelo de datos

---
+++ src/main/java/com/umg/vistaverde/model/Casa.java
@@ -0,0 +1,44 @@
+package com.umg.vistaverde.model;
+
+import java.util.ArrayList;
+
+public class Casa {
+
+    private int numeroCasa;
+    private Propietario propietario;
+    private ArrayList<Pago> pagos;
+
+    public Casa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+        this.propietario = null;
+        this.pagos = new ArrayList<>();
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public Propietario getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Propietario propietario) {
+        this.propietario = propietario;
+    }
+
+    public ArrayList<Pago> getPagos() {
+        return pagos;
+    }
+
+    public void setPagos(ArrayList<Pago> pagos) {
+        this.pagos = pagos;
+    }
+
+    public boolean tienePropietario() {
+        return propietario != null;
+    }
+}
