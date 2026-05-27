f494b9fab39f2cb4bbcb040701f1a5dfbfd2daec
larguetam2 <larguetam2@miumg.edu.gt>
2026-05-27 15:31:48 -0600

    Completar funcionalidad de ConfiguracionCuotaFrame

--- src/main/java/com/umg/vistaverde/ui/ConfiguracionCuotaFrame.java
+++ src/main/java/com/umg/vistaverde/ui/ConfiguracionCuotaFrame.java
@@ -1,14 +1,24 @@
 
 package com.umg.vistaverde.ui;
+import com.umg.vistaverde.model.Condominio;
+import com.umg.vistaverde.service.CondominioService;
+import javax.swing.JOptionPane;
 
 
 public class ConfiguracionCuotaFrame extends javax.swing.JFrame {
 
-    /**
-     * Creates new form EstadoCuentaFrame
-     */
+   private Condominio condominio;
+   private CondominioService service;
+    
+    
     public ConfiguracionCuotaFrame() {
         initComponents();
+        
+        condominio = new Condominio();
+        service = new CondominioService(condominio);
+        
+        lblCuotaActual.setText("Cuota acutal: Q"+ service.obtenerCuotaActual());
+        
     }
 
    
@@ -17,32 +27,34 @@ public class ConfiguracionCuotaFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        lblCuotaActual = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         txtNuevaCuota = new javax.swing.JTextField();
         btnActualizarCuota = new javax.swing.JButton();
         btnVolver = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(204, 204, 255));
+        setBackground(new java.awt.Color(255, 255, 153));
+        setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(51, 102, 255));
         jLabel1.setText("Configuración de Cuota");
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel2.setText("Cuota Actual: Q1500.00");
+        lblCuotaActual.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        lblCuotaActual.setText("Cuota Actual: Q1500.00");
 
         jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel3.setText("Nueva Cuota");
 
-        txtNuevaCuota.setText("___________");
         txtNuevaCuota.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtNuevaCuotaActionPerformed(evt);
             }
         });
 
+        btnActualizarCuota.setBackground(new java.awt.Color(0, 153, 102));
+        btnActualizarCuota.setForeground(new java.awt.Color(255, 255, 255));
         btnActualizarCuota.setText("Actualizar Cuota");
         btnActualizarCuota.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -68,15 +80,23 @@ public class ConfiguracionCuotaFrame extends javax.swing.JFrame {
                 .addGap(61, 61, 61))
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnVolver)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(btnActualizarCuota)
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(jLabel3)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(txtNuevaCuota, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addComponent(jLabel2)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblCuotaActual)
+                        .addContainerGap(232, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtNuevaCuota, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(148, Short.MAX_VALUE))))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btnVolver))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(btnActualizarCuota)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -85,23 +105,52 @@ public class ConfiguracionCuotaFrame extends javax.swing.JFrame {
                 .addGap(38, 38, 38)
                 .addComponent(jLabel1)
                 .addGap(43, 43, 43)
-                .addComponent(jLabel2)
+                .addComponent(lblCuotaActual)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(txtNuevaCuota, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(btnActualizarCuota)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
                 .addComponent(btnVolver)
-                .addGap(23, 23, 23))
+                .addGap(15, 15, 15))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnActualizarCuotaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActualizarCuotaActionPerformed
-        // TODO add your handling code here:
+        
+        String textoCuota = txtNuevaCuota.getText();
+        
+        try {
+            
+        
+        double nuevaCuota = Double.parseDouble(textoCuota);
+        
+        if (nuevaCuota <= 0 ){
+            JOptionPane.showMessageDialog(this,"la cuota no puede ser cero o negativa");
+            
+            return;    
+        }
+        
+        int confirmacion = JOptionPane.showConfirmDialog(this, "¿Está seguro que desea cambiar la cuota a Q" + nuevaCuota +"?","Confirmar actualización", JOptionPane.YES_NO_OPTION);
+           
+        if (confirmacion == JOptionPane.YES_OPTION){
+            service.actualizarCuota(nuevaCuota);
+            lblCuotaActual.setText("Cuota actual: Q" + service.obtenerCuotaActual());
+            
+            JOptionPane.showMessageDialog(this,
+            "Cuota actualizada correctamente");
+        }
+        }
+        
+        catch (NumberFormatException e){
+            
+                JOptionPane.showMessageDialog(this, "Ingrese un número válido");
+                }
+        
     }//GEN-LAST:event_btnActualizarCuotaActionPerformed
 
     private void txtNuevaCuotaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNuevaCuotaActionPerformed
@@ -109,7 +158,7 @@ public class ConfiguracionCuotaFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_txtNuevaCuotaActionPerformed
 
     private void btnVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVolverActionPerformed
-        // TODO add your handling code here:
+        dispose();
     }//GEN-LAST:event_btnVolverActionPerformed
 
     /**
@@ -154,8 +203,8 @@ public class ConfiguracionCuotaFrame extends javax.swing.JFrame {
     private javax.swing.JButton btnActualizarCuota;
     private javax.swing.JButton btnVolver;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel lblCuotaActual;
     private javax.swing.JTextField txtNuevaCuota;
     // End of variables declaration//GEN-END:variables
 }
